814f35588ed622873f429fb2cae016f93884a63b
pprmint <29925944+pprmint@users.noreply.github.com>
2023-10-11 05:49:01 +0200

    Add some path redirects for quicker access.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/lorem",
+				destination: "/lorem-ipsum",
+				permanent: true,
+			},
+			{
+				source: "/filter",
+				destination: "/filter-color",
+				permanent: true,
+			},
+            {
+                source: "/cubic",
+                destination: "/cubic-bezier",
+                permanent: true,
+            },
+			{
+				source: "/easing",
+				destination: "/cubic-bezier",
+				permanent: true,
+			},
+			{
+				source: "/svg",
+				destination: "/svg-generators",
+				permanent: true,
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
