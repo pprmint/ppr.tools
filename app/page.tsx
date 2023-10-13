88138de6763918f71d3dc9bb3e25bf5249e10ef5
pprmint <29925944+pprmint@users.noreply.github.com>
2023-10-13 08:42:47 +0200

    Less condescending intro text.

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ export default function Home() {
 	return (
 		<main className="animate-page-in">
 			<section className="flex flex-col gap-3">
-				<h1 className="text-neutral-50 font-display font-medium text-3xl md:text-5xl">Hello world<span className="text-blue">.</span></h1>
+				<h1 className="text-neutral-50 font-display font-medium text-3xl md:text-5xl">
+					Hello world<span className="text-blue">.</span>
+				</h1>
 				<p>
-					This site contains some small tools that may help me with web development or other stuff. This
-					mostly serves as a way for me to access stuff I frequently need in one central place. Hence the
-					funny monospace font, because <i>~ooOOOooo~ code</i>
+					This site is a collection of various tools that help me (and maybe you) with web development and
+					other things.
 				</p>
-				<p>Also I find the design of some of those sites ugly.</p>
+				<p>
+					There may be other sites out there which do the same things as some of the tools here, sure, but
+					this serves as a central place to access everything I need, while also having the ability to customize and change stuff as needed.
+				</p>
 			</section>
 		</main>
 	);
